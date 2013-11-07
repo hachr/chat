ce30ec658a3f167b660f844d5dcb3686cbd80a72
Nhat <nhat@hachr.com>
2013-11-06 18:59:38 -0800

    got the pairing to work

--- echo.js
+++ echo.js
@@ -11,20 +11,83 @@ server.listen(port);
 
 console.log('http server listening on %d', port);
 
+
+var ALPHABET = '0123456789';
+var TOKEN_LENGTH = 3;
+
+function generateToken() {
+  var token = '';
+  var count = 0;
+  var char;
+  var lastChar;
+  while (count < TOKEN_LENGTH) {
+    // Generate a random value from the alphabet.
+    var index = Math.floor(Math.random() * ALPHABET.length);
+    char = ALPHABET[index];
+    if (char != lastChar) {
+      count += 1;
+      token += char;
+      lastChar = char;
+    }
+  }
+  return token;
+}
+
+var receiver = null;
+var remote = null;
+var paired = false;
+var token = null;
+var pairAttempt = 0;
+
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
 wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
 
     console.log('websocket connection open');
 
-    ws.on('close', function() {
-        console.log('websocket connection close');
-        clearInterval(id);
-    });
     ws.on('message', function(message){
-        ws.send(message);
+			console.log("received: " + message);
+//			ws.send(message);
+			if(message){
+				try {
+					var payload = JSON.parse(message);
+
+					var command = payload["CMD"];
+					if(command === "ID"){
+						if(payload["name"] === "receiver"){
+							receiver = ws;
+						}else if(payload["name"] === "remote"){
+							remote = ws;
+							token = generateToken();
+							ws.send('{"CMD":"ID","code":"' + token + '"}');
+						}
+					}else if(command === "PAIR" || pairAttempt >=10){
+						pairAttempt = pairAttempt + 1;
+						console.log("pair request for " + payload["code"] + " with generated code: " + token );
+						if(payload["code"] == token){
+							console.log("paired!");
+							paired = true;
+							remote.send('{"CMD": "PAIRED"}');
+							receiver.send('{"CMD": "PAIRED"}');
+						}
+					}else if(command === "INFO" && paired){
+						console.log("sending info to remote");
+						remote.send(message);
+					}
+				} catch (e) {
+					console.error("Cannot parse payload");
+				}
+			}
+
     });
+
+		ws.on('close', function(){
+			console.log('websocket connection close');
+			if(ws === remote){
+				paired = false;
+				token = null;
+				remote = null;
+				pairAttempt = 0;
+			}
+		});
 });
